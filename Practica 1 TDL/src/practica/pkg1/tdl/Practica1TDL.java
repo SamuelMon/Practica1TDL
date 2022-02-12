47ef8787b752b12c98ddc907176ea3a210e9a236
SamuelMon <64022058+SamuelMon@users.noreply.github.com>
2022-02-11 19:00:05 -0500

    Lo del inicio
    Na

---
+++ Practica 1 TDL/src/practica/pkg1/tdl/Practica1TDL.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica.pkg1.tdl;
+
+/**
+ *
+ * @author monto
+ */
+public class Practica1TDL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI interfaz = new GUI();
+        interfaz.setVisible(true);
+        interfaz.setLocationRelativeTo(null);
+    }
+    
+}
